07643195393ade990184d0d788ccd7baab1b7189
Andrew Rodriguez <andrewrb22@gmail.com>
2022-01-22 13:16:50 -0500

    Clean code and comments added

--- HW2.c
+++ HW2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 // Write a program that converts 27° from degrees Fahrenheit (F) to degrees Celsius (C) 
 // using the following formula: 
 // C = ( F - 32) / 1.8 
@@ -7,17 +8,16 @@
 
 int main(){
 
+// Values in float to be able to enter decimal number
 float F = 27;
 float C;
 
+//Formula to convert temperatures from C to F
 C = (F - 32) / 1.8; 
 
+// Printing finla result 
+
 printf(" %.2f degrees F is equal to %.2f degree C", F,C  );
-
-
-
-
-
 
 return 0;
 
